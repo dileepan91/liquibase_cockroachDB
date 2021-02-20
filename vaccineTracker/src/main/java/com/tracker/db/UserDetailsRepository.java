a3c3aa4f995c09adf612a0f3860ec0a1611e5a97
dileepan <61397718+dileepan91@users.noreply.github.com>
2021-02-20 11:49:40 -0600

    Delete vaccineTracker directory

--- vaccineTracker/src/main/java/com/tracker/db/UserDetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tracker.db;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserDetailsRepository extends CrudRepository<Users, String> {
-}
